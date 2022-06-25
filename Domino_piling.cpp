8dcd0f68fe21a7bc4d2da51fd65831f7f73e4bdf
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-25 05:53:14 -0700

    codeforces 50A - Domino piling

---
+++ Domino_piling.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main (){
+    int N,M;
+ 
+    cin >> N,
+    cin >> M;
+    int area = N * M;
+    int domino;
+    domino = area / 2 ;
+ 
+    cout << domino << endl;
+ 
+    return 0;
+}
